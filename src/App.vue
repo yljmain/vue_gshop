f46db40024eb791cd879505b7bc99d9c4606328d
ylj <920575414@qq.com>
2019-12-30 20:37:41 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <FooterGuider v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGuider from './components/FooterGuider/FooterGuider'
+// 向外默认暴露一个配置对象
+export default {
+  components: {
+    FooterGuider
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .app
+    width 100%
+    height 100%
+    background #f5f5f5
+</style>
